92890204b278abdd3a3d1673bd41bbf9fb978e20
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-09-27 08:40:14 -0600

    Specify types when destructuring the binary packet

--- ch2/ex6_test.exs
+++ ch2/ex6_test.exs
@@ -2,20 +2,20 @@ defmodule Ex6 do
   def parse(packet) do
     << header :: binary-size(20), _content :: binary >> = packet
     <<
-      version :: size(4),
-      header_length :: size(4),
-      tos :: size(8),
-      total_length :: size(16),
-      fragment_id :: size(16),
-      _evil_bit :: size(1),
-      dont_fragment :: size(1),
-      more_fragments :: size(1),
-      fragment_offset :: size(13),
-      ttl :: size(8),
-      protocol :: size(8),
-      header_checksum :: size(16),
-      source :: size(32),
-      destination :: size(32),
+      version :: integer-size(4),
+      header_length :: integer-size(4),
+      tos :: integer-size(8), # I think maybe this should stay a binary?
+      total_length :: integer-size(16),
+      fragment_id :: binary-size(2),
+      _evil_bit :: integer-size(1),
+      dont_fragment :: integer-size(1),
+      more_fragments :: integer-size(1),
+      fragment_offset :: integer-size(13),
+      ttl :: integer-size(8),
+      protocol :: integer-size(8),
+      header_checksum :: binary-size(2),
+      source :: binary-size(4),
+      destination :: binary-size(4),
       _options :: binary
     >> = header
     %{
@@ -30,13 +30,17 @@ defmodule Ex6 do
       ttl: ttl,
       protocol: protocol,
       header_checksum: header_checksum,
-      source: source, #|> IP.Address.from_binary!() |> IP.Address.to_string(),
-      destination: destination #|> IP.Address.from_binary!() |> IP.Address.to_string()
+      source: parse_ipv4_address(source),
+      destination: parse_ipv4_address(destination)
     }
   end
 
   defp parse_bool(0), do: false
   defp parse_bool(1), do: true
+
+  # For some reason I couldn't get IP.Address to load ¯\_(ツ)_/¯
+  defp parse_ipv4_address(<< a::8, b::8, c::8, d::8 >>),
+    do: "#{a}.#{b}.#{c}.#{d}"
 end
 
 ExUnit.start()
@@ -55,15 +59,15 @@ defmodule Ex6Test do
     assert parsed_packet.header_length == 5
     assert parsed_packet.tos == 0
     assert parsed_packet.total_length == 68
-    #assert parsed_packet.fragment_id == << 173, 11 >>
+    assert parsed_packet.fragment_id == << 173, 11 >>
     assert parsed_packet.dont_fragment == false
     assert parsed_packet.more_fragments == false
     assert parsed_packet.fragment_offset == 0
     assert parsed_packet.ttl == 64
     assert parsed_packet.protocol == 17
-    #assert parsed_packet.header_checksum == << 114, 114 >>
-    #assert parsed_packet.source == "172.20.2.253"
-    #assert parsed_packet.destination == "172.20.0.6"
+    assert parsed_packet.header_checksum == << 114, 114 >>
+    assert parsed_packet.source == "172.20.2.253"
+    assert parsed_packet.destination == "172.20.0.6"
   end
 end
 
